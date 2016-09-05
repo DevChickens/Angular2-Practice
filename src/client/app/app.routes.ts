4edb1d5ec5da758fc4326eab1a5ecad18c22db7e
taehoon.kim <taehoon.kim@traport.com>
2016-09-05 09:27:24 +0900

    fix( 추가 삭제 )
    - Maptest Component 삭제
    - VocaMobileComponent 추가

--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -1,15 +1,15 @@
-/* 테스트 */
 import { Routes } from '@angular/router';
 import { HomeRoutes } from './+home/index';
 import { DatepickerTestRoutes } from './+datepicker/datepicker-test.routes';
 import { SliderTestRoutes } from './+slider/index';
 import { NumberPickerTestRoutes } from './+numberpicker/index';
-import { MapTestRoutes } from './+map/index';
+// import { MapTestRoutes } from './+map/index/';
+import { VocaMobileRoutes } from './+vocamobile/index';
 
 export const routes: Routes = [
   ...HomeRoutes,
   ...SliderTestRoutes,
   ...NumberPickerTestRoutes,
-  ...MapTestRoutes,
-  ...DatepickerTestRoutes
+  ...DatepickerTestRoutes,
+  ...VocaMobileRoutes
 ];
